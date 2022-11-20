f454a7009f6691b13f81957926834398bf5f4c53
Matt Davis <matteius@gmail.com>
2022-11-19 23:35:50 -0500

    add missing files.

---
+++ image-loader.config.js
@@ -0,0 +1,5 @@
+import { imageLoader } from "next-image-loader/build/image-loader";
+
+// write self-define a custom loader
+// (resolverProps: { src: string; width: number; quality?: number }) => string
+imageLoader.loader = ({ src, width }) => `${src}?w=${width}`;

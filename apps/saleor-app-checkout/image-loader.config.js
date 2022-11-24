08915e7f496e768fe487176680a2fe3ae209a6d3
Matt Davis <matteius@gmail.com>
2022-11-24 00:42:44 -0500

    add missing file.

---
+++ apps/saleor-app-checkout/image-loader.config.js
@@ -0,0 +1,8 @@
+import { imageLoader } from "next-image-loader/image-loader";
+
+// write self-define a custom loader
+// (resolverProps: { src: string; width: number; quality?: number }) => string
+imageLoader.loader = ({ src, width }) => {
+  const filename = src.substring(src.lastIndexOf("/") + 1);
+  return `https://cdn.mattscoinage.com/mattscoinage/products/${filename}?w=${width}`;
+};

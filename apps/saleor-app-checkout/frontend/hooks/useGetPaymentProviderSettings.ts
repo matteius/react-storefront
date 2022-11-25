a877f44da06c70998c6535f168ecf4f94f318ac0
Matt Davis <matteius@gmail.com>
2022-11-24 19:44:29 -0500

    Corrections

--- apps/saleor-app-checkout/frontend/hooks/useGetPaymentProviderSettings.ts
+++ apps/saleor-app-checkout/frontend/hooks/useGetPaymentProviderSettings.ts
@@ -24,7 +24,7 @@ export const useGetPaymentProviderSettings = <TArgs>(
     args: {
       ...optionalProps?.args,
       saleorApiUrl,
-      token: app.getState().token,
+      token: app?.getState().token,
     },
   });
 
